c4f6c18f2cc39bc63a50619a74a5235e6454e2a5
jaeyoung <jaeyoung0509@naver.com>
2022-06-27 23:17:02 +0900

    create test code added

---
+++ controller/product.js
@@ -0,0 +1,11 @@
+const productModel = require("../models/Product");
+
+exports.createProduct = async (req,res,next) => {
+    try{
+        const createdProduct =  await productModel.create(req.body);
+        res.status(201).json(createdProduct);   
+    }
+    catch (error) {
+        next(error)
+    }
+};
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const HOST = '0.0.0.0'
-const PORT =5000;
+const PORT =9999;
 const express = require("express");
 const app = express();
+const mongoose =  require('mongoose')
+const productRoutes =  require('./routes')
+require("dotenv").config();
 
 app.use(express.json())
-
-app.get("/" ,(req ,res)=>{
-    req.send("hello world");
-})
+mongoose.connect(process.env.MONGO_DB_URL , {
+    useNewUrlParser :true 
+}).then(() => console.log("Mongoddb Connected"))
+.catch(err => console.log(err));
+app.unsubscribe("/api/products" ,productRoutes)
 
 app.listen(PORT)
+
